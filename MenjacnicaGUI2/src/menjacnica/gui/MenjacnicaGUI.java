e7a626cb8f79166028cc525adf5cfa210f56b71f
Dario <dariojahic95@gmail.com>
2016-05-01 17:24:09 +0200

    Napravljen MenjacnicaGUI sa svojim komponentama.

--- MenjacnicaGUI2/src/menjacnica/gui/MenjacnicaGUI.java
+++ MenjacnicaGUI2/src/menjacnica/gui/MenjacnicaGUI.java
@@ -6,10 +6,27 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Dimension;
+import javax.swing.border.TitledBorder;
+
+import menjacnica.models.MenjacnicaTableModel;
+
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
 
 public class MenjacnicaGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JPanel eastPanel;
+	private JPanel southPanel;
+	private JButton btnDodajKurs;
+	private JButton btnIzbrisiKurs;
+	private JButton btnIzvrsiIzmenu;
+	private JScrollPane scrollPane;
+	private JTable table;
+	private JTextArea textArea;
 
 	/**
 	 * Launch the application.
@@ -31,12 +48,75 @@ public class MenjacnicaGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public MenjacnicaGUI() {
+		setTitle("Menjacnica");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.add(getEastPanel(), BorderLayout.EAST);
+		contentPane.add(getSouthPanel(), BorderLayout.SOUTH);
+		contentPane.add(getScrollPane(), BorderLayout.CENTER);
 	}
 
+	private JPanel getEastPanel() {
+		if (eastPanel == null) {
+			eastPanel = new JPanel();
+			eastPanel.setPreferredSize(new Dimension(100, 10));
+			eastPanel.add(getBtnDodajKurs());
+			eastPanel.add(getBtnIzbrisiKurs());
+			eastPanel.add(getBtnIzvrsiIzmenu());
+		}
+		return eastPanel;
+	}
+	private JPanel getSouthPanel() {
+		if (southPanel == null) {
+			southPanel = new JPanel();
+			southPanel.setBorder(new TitledBorder(null, "Status", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			southPanel.setPreferredSize(new Dimension(10, 40));
+			southPanel.add(getTextArea());
+		}
+		return southPanel;
+	}
+	private JButton getBtnDodajKurs() {
+		if (btnDodajKurs == null) {
+			btnDodajKurs = new JButton("Dodaj kurs");
+		}
+		return btnDodajKurs;
+	}
+	private JButton getBtnIzbrisiKurs() {
+		if (btnIzbrisiKurs == null) {
+			btnIzbrisiKurs = new JButton("Izbrisi kurs");
+		}
+		return btnIzbrisiKurs;
+	}
+	private JButton getBtnIzvrsiIzmenu() {
+		if (btnIzvrsiIzmenu == null) {
+			btnIzvrsiIzmenu = new JButton("Izvrsi izmenu");
+		}
+		return btnIzvrsiIzmenu;
+	}
+	private JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setViewportView(getTable());
+		}
+		return scrollPane;
+	}
+	private JTable getTable() {
+		if (table == null) {
+			table = new JTable();
+			MenjacnicaTableModel mtm = new MenjacnicaTableModel();
+			table.setModel(mtm);
+		}
+		return table;
+	}
+	private JTextArea getTextArea() {
+		if (textArea == null) {
+			textArea = new JTextArea();
+			textArea.setPreferredSize(new Dimension(399, 20));
+		}
+		return textArea;
+	}
 }
